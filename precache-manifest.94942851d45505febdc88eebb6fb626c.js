51c4c1fd9017810a353ef9f9be59a842faaa660b
Daniel Vargas <50393869+dv1runner@users.noreply.github.com>
2020-04-09 15:28:13 -0700

    Updates

---
+++ precache-manifest.94942851d45505febdc88eebb6fb626c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a86b64b2092ccbee218d023c060f96",
+    "url": "/TodoList-App/index.html"
+  },
+  {
+    "revision": "9c57171e0b1b8f9695af",
+    "url": "/TodoList-App/static/css/main.861914d6.chunk.css"
+  },
+  {
+    "revision": "c78dff2b11fa51a66ed2",
+    "url": "/TodoList-App/static/js/2.cb4c827f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList-App/static/js/2.cb4c827f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c57171e0b1b8f9695af",
+    "url": "/TodoList-App/static/js/main.a23391ce.chunk.js"
+  },
+  {
+    "revision": "795d424a9847f9e4e1bf",
+    "url": "/TodoList-App/static/js/runtime-main.781ddb1f.js"
+  }
+]);
